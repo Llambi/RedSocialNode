822c54f3edb815a536f84014ab5fed011af96922
Hugo Perez Fernandez <hugopf.mail@gmail.com>
2018-04-16 23:25:34 +0200

    [INICIO] Arrancado el proyecto de RedSocial
    LOgica minima para arrancar el proyecto de la redSocial

---
+++ app.js
@@ -0,0 +1,42 @@
+//Módulos
+var express = require('express');
+var app = express();
+
+var crypto = require('crypto');
+var mongo = require('mongodb');
+var swig = require("swig");
+
+var bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+var gestorBD = require("./modules/gestorBD.js");
+gestorBD.init(app, mongo);
+
+app.use(express.static("public"));
+
+//Variables
+app.set("port", 8081);
+app.set("db", "mongodb://uo250708:EIISDI2018$@ds147469.mlab.com:47469/redsocial");
+app.set("clave", "abcdefg");
+app.set("crypto", crypto);
+
+app.get('/', function (req, res) {
+    res.redirect('/redSocial');
+});
+
+//Controlar error
+app.use(function (err, req, res, next) {
+    console.log("Error producido: " + err); // we log the error in our db
+    if (!res.headersSent) {
+        res.status(400);
+        res.send("Recurso no disponible");
+    }
+});
+
+//Lanzar aplicacion
+app.listen(app.get('port'), function () {
+    console.log("Servidor activo en puerto: " + app.get("port"));
+});
