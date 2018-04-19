275aa4b467d758fafc695388a7b41144fc47e7d7
Hugo Perez Fernandez <hugopf.mail@gmail.com>
2018-04-19 17:21:03 +0200

    [FIN] Sesion y Routes
    Acabadas ambas tareas ademas de añadida la funcionalidad de mostrar menus en funcion del estado.

--- routes/rUsuarios.js
+++ routes/rUsuarios.js
@@ -56,9 +56,7 @@ module.exports = function (app, swig, gestorBD) {
                     "&tipoMensaje=alert-danger ");
 
             } else {
-                var usuario={name:usuarios[0].name, email:usuarios[0].email};
-                req.session.usuario = usuarios[0].email;
-                res.usuario= usuario;
+                req.session.usuario = {name:usuarios[0].name, email:usuarios[0].email};
                 res.redirect("/usuarios");
             }
         });
@@ -66,7 +64,7 @@ module.exports = function (app, swig, gestorBD) {
 
     app.get('/logout', function (req, res) {
         req.session.usuario = null;
-        res.redirect("/redsocial" +
+        res.redirect("/" +
             "?mensaje=Desconectado correctamente" +
             "&tipoMensaje=alert-success ");
     });
@@ -93,11 +91,11 @@ module.exports = function (app, swig, gestorBD) {
 
                 var pgUltima = Math.ceil(total / 4);
                 var respuesta = swig.renderFile('views/bUsuarios.html', {
+                    usuario: req.session.usuario,
                     usuarios: usuarios,
                     pgActual: pg,
                     pgUltima: pgUltima
                 });
-                console.log(respuesta.usuarios);
                 res.send(respuesta);
             }
         });
